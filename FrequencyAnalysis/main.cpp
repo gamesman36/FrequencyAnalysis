82992f360ea8a505bbdfc6dd38aa7990b976837c
Daniel Andreas Wang <gamesman36@outlook.com>
2023-09-10 20:35:25 +0200

    Update main.cpp
    Sorting code

--- FrequencyAnalysis/main.cpp
+++ FrequencyAnalysis/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cctype>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 int main() {
     std::map<char, int> letterCounts;
@@ -13,7 +15,13 @@ int main() {
         }
     }
 
-    for (const auto& pair : letterCounts) {
-        std::cout << pair.first << ": " << pair.second << "\n";
+    std::vector<std::pair<char, int>> sortedLetterCounts(letterCounts.begin(), letterCounts.end());
+
+    std::sort(sortedLetterCounts.begin(), sortedLetterCounts.end(), [](const auto& a, const auto& b) {
+        return a.second > b.second;
+    });
+
+    for (const auto& entry : sortedLetterCounts) {
+        std::cout << entry.first << ": " << entry.second << "\n";
     }
 }
